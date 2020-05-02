28572df53b359035131e99d005c5eabb68bd99d5
Xavier Francisco <xavier.n.francisco@gmail.com>
2020-05-02 15:41:45 +0100

    Update sync-all

--- .circleci/sync-all.sh
+++ .circleci/sync-all.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-git log | grep git-subtree-dir | tr -d ' ' | cut -d ":" -f2 | sort | uniq | xargs -I {} bash -c 'if [ -d $(git rev-parse --show-toplevel)/{} ] ; then echo {}; fi' | xargs -L1 -I% git subtree push --prefix=% ssh+git://aur@aur.archlinux.org/%.git master
+subtrees(){
+  git log | grep git-subtree-dir | tr -d ' ' | cut -d ":" -f2 | sort | uniq | xargs -I {} bash -c 'if [ -d $(git rev-parse --show-toplevel)/{} ] ; then echo {}; fi'
+}
+
+subtrees | xargs -L1 -I% git subtree push --prefix=% ssh+git://aur@aur.archlinux.org/%.git master
+subtrees | xargs -L1 -I% git subtree pull --prefix=% https://aur@aur.archlinux.org/%.git master --squash
